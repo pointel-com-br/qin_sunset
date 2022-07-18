b64eefc9af41bd2b23d7fd07babdab5d4f75efab
Éverton M. Vieira <emuvi@outlook.com.br>
2022-07-18 19:52:05 +0000

    setups the ports to test and production

--- put-on-prod.py
+++ put-on-prod.py
@@ -6,5 +6,6 @@ import shutil
 qin_root = os.environ['QIN_ROOT']
 os.system("mvn clean install")
 shutil.copytree("./dist", f"{qin_root}/Prod", dirs_exist_ok=True)
+shutil.copy("./setup-prod.json", f"{qin_root}/Prod/setup.json")
 shutil.copy("./start.bat", f"{qin_root}/Prod/start.bat")
 shutil.copy("./start.sh", f"{qin_root}/Prod/start.sh")
--- put-on-test.py
+++ put-on-test.py
@@ -6,5 +6,6 @@ import shutil
 qin_root = os.environ['QIN_ROOT']
 os.system("mvn clean install")
 shutil.copytree("./dist", f"{qin_root}/Test", dirs_exist_ok=True)
+shutil.copy("./setup-test.json", f"{qin_root}/Test/setup.json")
 shutil.copy("./start.bat", f"{qin_root}/Test/start.bat")
 shutil.copy("./start.sh", f"{qin_root}/Test/start.sh")
