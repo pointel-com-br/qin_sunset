883d37823dcd6ed55c00558f20bc2acfa3ed7bd2
Éverton M. Vieira <emuvi@outlook.com.br>
2022-07-28 10:04:21 -0300

    puts an guard to check if the directory exists

--- src/main/java/br/net/pin/qin_sunset/work/Utils.java
+++ src/main/java/br/net/pin/qin_sunset/work/Utils.java
@@ -14,10 +14,12 @@ public class Utils {
 
   public static String listFolders(File onDir) {
     var result = new StringBuilder();
-    for (var file : onDir.listFiles()) {
-      if (file.isDirectory()) {
-        result.append(file.getName());
-        result.append("\n");
+    if (onDir.exists()) {
+      for (var file : onDir.listFiles()) {
+        if (file.isDirectory()) {
+          result.append(file.getName());
+          result.append("\n");
+        }
       }
     }
     return result.toString();
